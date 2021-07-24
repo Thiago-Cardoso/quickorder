c8747679ab5dcb4514b79dbb465ab85690474f98
Pompermaier <8593387+Pompermaier@users.noreply.github.com>
2021-07-24 16:35:09 -0400

    Update start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export RUBY_ENVIRONMENT="development"
+export RAILS_ENV=development
+
+cd /app
+rm -rf tmp/*
+#rm -rf log/*
+bundle install
+#bundle exec whenever --update-crontab --set environment=development
+bundle exec rails assets:precompile
+bundle exec rails s -b 0.0.0.0
