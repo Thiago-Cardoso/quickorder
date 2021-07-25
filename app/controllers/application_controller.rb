178c7bd50f79e75b2e7622f76dc2f318c912634e
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-07-25 03:06:14 -0300

    feat: configuration of devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_employee!
+  protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
 end
