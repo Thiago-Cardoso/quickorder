9e00f468eecdc39d6ef362c494794dd0c5ffdf9f
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-07-25 11:36:23 -0300

    chore: adjustment

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   #devise_for :employees, :controllers => { registrations: 'registrations' }
   devise_for :employees do
     get '/employees/sign_out' => 'devise/sessions#destroy'
+    get '/employees/reset_password'
   end
+
   get 'dashboard', to: 'dashboard#index'
   namespace :dashboard do
     resources :categories
