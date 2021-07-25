ec6c3a58b11ec2865d014b9307f5ec7ac19cc9fd
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-07-25 09:51:06 -0300

    chore: initial logic in ability

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,7 @@
 class Employee < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :order, optional: true
+
+  enum type: { admin: 0, waitress: 1, cooker: 2}
 end
