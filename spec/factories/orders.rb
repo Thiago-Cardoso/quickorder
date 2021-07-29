1adfdcbe359c023458d7e8adeec15bfc050182a6
Thiago-Cardoso <thiagocardosooti@gmail.com>
2021-07-29 00:30:27 -0300

    test: models and factories

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :order do
     client_name { Faker::Name.name }
     table_number { "MyString" }
+    situation { "Na fila"}
+    employee
   end
 end
--- spec/models/product_order_spec.rb
+++ spec/models/product_order_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ProductOrder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with quantitie' do
+    product_order = create(:product_order)
+    expect(product_order).to be_valid
+  end
+
+  context 'Validates' do
+    it { is_expected.to validate_presence_of(:quantitie)}
+  end
+
+  context 'Association' do
+    it { is_expected.to belong_to(:product)}
+    it { is_expected.to belong_to(:order)}
+  end
 end
