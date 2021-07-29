137b0c7a274f7d124b9f78ceaa0ab14d6205647b
RaquelLima7 <raquellimadev@gmail.com>
2021-07-29 13:27:04 -0300

    add scope

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,21 @@
 class DashboardController < ApplicationController
   layout 'dashboard'
   def index
+    @orders = Order.all
+    @orders_fila = Order.na_fila
+    # @orders = Order.na_fila..where(employee_id: current_employee)
+    @orders_em_andamento = Order.em_andamento
+    @orders_concluido = Order.concluido
+    @orders_cancelado = Order.cancelado
 
+    # @sum_orders = ProductOrder.joins('inner join products on products.id = product_orders.product_id').sum(:price).round(2), depois só passar um where created_at pra fazer o filtro
+
+    # if current_employee == admin
+    #   @orders = Order.na_fila.count
+    # else
+    #   @orders = Order.na_fila.where(employee_id: current_employee)
+    
+    # tenta algo assim pra vc validar o usuario no controller
+    # se for admin traz tudo se não traz do usuario logado
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -11,4 +11,10 @@ class Order < ApplicationRecord
 
   # Enumerate
   enum situation: { 'Na fila': 0, 'Em andamento': 1, 'Concluído': 2, 'Cancelado': 3 }
+
+  scope :na_fila, ->{ where(situation: 0)}
+  scope :em_andamento, ->{ where(situation: 1)}
+  scope :concluido, ->{ where(situation: 2)}
+  scope :cancelado, ->{ where(situation: 3)}
+
 end
