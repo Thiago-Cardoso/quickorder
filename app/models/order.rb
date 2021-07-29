ce7601aee29e8112b855df429e6e9f26219ad226
RaquelLima7 <raquellimadev@gmail.com>
2021-07-29 17:34:46 -0300

    fix conflict

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,12 +10,11 @@ class Order < ApplicationRecord
   validates :situation, presence: true
 
   # Enumerate
-  enum situation: { 'Na fila': 0, 'Em andamento': 1, 'Concluído': 2, 'Cancelado': 3 }
+  enum situation: { queue: 0, progress: 1, concluded: 2, canceled: 3 }
 
   scope :na_fila, ->{ where(situation: 0)}
   scope :em_andamento, ->{ where(situation: 1)}
   scope :concluido, ->{ where(situation: 2)}
   scope :cancelado, ->{ where(situation: 3)}
 
-  enum situation: { queue: 0, progress: 1, concluded: 2, canceled: 3 }
 end
