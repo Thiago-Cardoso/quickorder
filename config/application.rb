59ca666f1376c12b3e8f6ac62ebcd984a5243d37
Fabio Vilhena <fvilhena@gmail.com>
2021-07-31 22:56:20 -0300

    Internationalization Setup

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module Quickorder
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.i18n.default_locale = 'pt-BR'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
